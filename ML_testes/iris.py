9dc97e5b9cea3430044b62aa122d6c049d4cf715
Matos-V <vitor.matos@ee.ufcg.edu.br>
2021-01-05 11:37:10 -0300

    carregamento do conjunto de dados

--- ML_testes/iris.py
+++ ML_testes/iris.py
@@ -3,4 +3,10 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import tensorflow_datasets as tfds
+# %%
+dataset,info= tfds.load('wine_quality',split='train',with_info=True)
+# %%
+dataset = tfds.as_dataframe(dataset.take(4500),info)
+dataset.head()
 # %%
