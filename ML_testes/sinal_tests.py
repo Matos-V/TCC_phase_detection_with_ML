f8f5d1ae5586d51457ba7774ce98d0acf1fce4d1
Matos-V <vitor.matos@ee.ufcg.edu.br>
2021-01-23 22:00:52 -0300

    testes de deep learning com o dataset inicial

--- ML_testes/sinal_tests.py
+++ ML_testes/sinal_tests.py
@@ -8,8 +8,8 @@ import seaborn as sns
 dados = '..\\Testes_sinais_digitais\\'
 
 # %%
-features = pd.read_pickle(dados+'features.pkl')
-labels = pd.read_pickle(dados+'labels.pkl')
+features = pd.read_pickle(dados+'features.pkl')[0:5000]
+labels = pd.read_pickle(dados+'labels.pkl')[0:5000]
 # %%
 features.head()
 # %%
@@ -40,4 +40,42 @@ r2_score(y_test, preds)
 np.sqrt(mean_squared_error(y_test, preds))
 # %%
 mean_absolute_error(y_test,preds)
+# %%
+import tensorflow as tf
+from tensorflow.keras.layers import Dense,Dropout
+from tensorflow.keras.models import Sequential
+# %%
+model = Sequential()
+
+model.add(Dense(30,activation='relu'))
+
+model.add(Dense(30,activation='relu'))
+model.add(Dropout(0.5))
+
+model.add(Dense(30,activation='relu'))
+model.add(Dropout(0.5))
+
+model.add(Dense(1))
+
+model.compile(optimizer='adam',loss='mse')
+
+# %%
+model.fit(x=X_train, 
+          y=y_train, 
+          epochs=100,
+          validation_data=(X_test, y_test), verbose=1,
+          )
+# %%
+preds2 = model.predict(X_test)
+# %%
+r2_score(y_test, preds2)
+# %%
+np.sqrt(mean_squared_error(y_test, preds2))
+# %%
+mean_absolute_error(y_test,preds2)
+# %%
+plt.plot(y_test,y_test)
+plt.plot(y_test,preds2,'ro')
+plt.legend(['Valores Reais','Valores Preditos'])
+plt.title('Análise entre valores reais e predições com DP')
 # %%
